7bb90c3d24caada14d7b76f6095e684ad8126c7c
Andrey Kuzmin <unsoundscapes@gmail.com>
2018-08-26 15:41:32 +0200

    Update the release script

--- release.sh
+++ release.sh
@@ -3,14 +3,14 @@ set -euxo pipefail
 
 rm -rf release || exit 0;
 
-elm-package bump
+elm bump
 
-version=$(grep -m1 version elm-package.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+version=$(grep -m1 version elm.json | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
 git commit -a -m "Bump to $version"
 git push
 
-cleanup="examples gh-pages.sh tests release.sh"
+cleanup="examples gh-pages.sh tests release.sh src/Fixtures"
 last_commit=$(git rev-parse HEAD)
 
 git clone --reference . git@github.com:w0rm/elm-physics.git release
@@ -21,5 +21,5 @@ git clone --reference . git@github.com:w0rm/elm-physics.git release
   git commit -m "Cleanup and release $version"
   git tag -a $version -m "Release $version"
   git push origin $version
-  elm-package publish
+  elm publish
 )
