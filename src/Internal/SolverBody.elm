0cc60903f1aea285d5bd0131f5816bece729eaee
Andrey Kuzmin <andrey.kuzmin@soundcloud.com>
2019-04-07 11:37:16 +0200

    Inline vector computation on the hot path

--- src/Internal/SolverBody.elm
+++ src/Internal/SolverBody.elm
@@ -30,12 +30,22 @@ addToWlambda : Float -> JacobianElement -> SolverBody data -> SolverBody data
 addToWlambda deltalambda { spatial, rotational } { body, vlambda, wlambda } =
     { body = body
     , vlambda =
-        spatial
-            |> Vec3.scale (deltalambda * body.invMass)
-            |> Vec3.add vlambda
+        {- spatial
+           |> Vec3.scale (deltalambda * body.invMass)
+           |> Vec3.add vlambda
+        -}
+        { x = spatial.x * deltalambda * body.invMass + vlambda.x
+        , y = spatial.y * deltalambda * body.invMass + vlambda.y
+        , z = spatial.z * deltalambda * body.invMass + vlambda.z
+        }
     , wlambda =
-        rotational
-            |> Mat3.transform body.invInertiaWorld
-            |> Vec3.scale deltalambda
-            |> Vec3.add wlambda
+        {- rotational
+           |> Mat3.transform body.invInertiaWorld
+           |> Vec3.scale deltalambda
+           |> Vec3.add wlambda
+        -}
+        { x = (body.invInertiaWorld.m11 * rotational.x + body.invInertiaWorld.m12 * rotational.y + body.invInertiaWorld.m13 * rotational.z) * deltalambda + wlambda.x
+        , y = (body.invInertiaWorld.m21 * rotational.x + body.invInertiaWorld.m22 * rotational.y + body.invInertiaWorld.m23 * rotational.z) * deltalambda + wlambda.y
+        , z = (body.invInertiaWorld.m31 * rotational.x + body.invInertiaWorld.m32 * rotational.y + body.invInertiaWorld.m33 * rotational.z) * deltalambda + wlambda.z
+        }
     }
