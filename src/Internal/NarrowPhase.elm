ac3e37ccad6a9aa3b343ab4387e7c85d6e6bc6ef
Andrey Kuzmin <unsoundscapes@gmail.com>
2020-04-03 23:41:22 +0200

    Remove foldls

--- src/Internal/NarrowPhase.elm
+++ src/Internal/NarrowPhase.elm
@@ -15,15 +15,35 @@ import Physics.Coordinates exposing (WorldCoordinates)
 
 getContacts : List (Shape WorldCoordinates) -> List (Shape WorldCoordinates) -> List Contact
 getContacts shapes1 shapes2 =
-    List.foldl
-        (\shape1 currentContactEquations1 ->
-            List.foldl
-                (\shape2 -> addShapeContacts shape1 shape2)
-                currentContactEquations1
+    case shapes1 of
+        shape1 :: remainingShapes1 ->
+            getContactsHelp shape1 remainingShapes1 shapes2 shapes2 []
+
+        [] ->
+            []
+
+
+getContactsHelp : Shape WorldCoordinates -> List (Shape WorldCoordinates) -> List (Shape WorldCoordinates) -> List (Shape WorldCoordinates) -> List Contact -> List Contact
+getContactsHelp shape1 currentShapes1 currentShapes2 shapes2 result =
+    case currentShapes2 of
+        shape2 :: remainingShapes2 ->
+            getContactsHelp shape1
+                currentShapes1
+                remainingShapes2
                 shapes2
-        )
-        []
-        shapes1
+                (addShapeContacts shape1 shape2 result)
+
+        [] ->
+            case currentShapes1 of
+                newShape1 :: remainingShapes1 ->
+                    getContactsHelp newShape1
+                        remainingShapes1
+                        shapes2
+                        shapes2
+                        result
+
+                [] ->
+                    result
 
 
 addShapeContacts : Shape WorldCoordinates -> Shape WorldCoordinates -> List Contact -> List Contact
