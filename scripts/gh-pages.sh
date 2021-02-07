2ebef286aa00fa145c5e1b230905f11a390bd926
Andrey Kuzmin <unsoundscapes@gmail.com>
2021-02-07 21:36:10 +0100

    Fix the paths

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -32,7 +32,7 @@ for example in *.elm; do
   elm make $example --optimize --output ../../gh-pages$version_path/examples/$lower/index.html
 done
 cp Duckling.obj.txt Duckling.png ../../gh-pages$version_path/examples/duckling
-cp ../elm-physics.gif ../../gh-pages$version_path/examples
+cp ../../elm-physics.gif ../../gh-pages$version_path/examples
 
 cd ../../gh-pages
 git add .
